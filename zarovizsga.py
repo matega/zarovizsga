80b3475718fcb7268318bc33cb372aea86068f9c
Galambos Máté <matega@mensa.hu>
2017-05-06 20:56:31 +0200

    Command line args, .tex output

--- zarovizsga.py
+++ zarovizsga.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
-import requests, re, getpass, configparser
+import requests, re, getpass, configparser, json, argparse, sys
 from bs4 import BeautifulSoup
 
 fcsop_re=re.compile(r'fcsop\[fcsop\]=(\d+)');
@@ -14,13 +14,18 @@ class LoginException(Exception):
 
 def login():
     global jar
+    debug("Preparing to log in", 3)
     try:
         cp=configparser.RawConfigParser()
         cp.read('login.ini')
         login={'login[username]':cp.get('zarovizsga', 'user'),'login[password]':cp.get('zarovizsga','pass'),'login[loggedin]':'BELÉP'}
+        debug("Login info read from file", 2)
     except:
+        debug("Failed to read login.ini", 2)
         login={'login[username]':input('E-mail cím? '),'login[password]':getpass.getpass('Jelszó? '),'login[loggedin]':'BELÉP'}
+    debug("Login array: "+str(login), 3)
     try:
+        debug("Attempting login", 2)
         r=requests.post('http://aok.zarovizsga.hu/login_box/',data=login)
         r.encoding="UTF-8"
         soup=BeautifulSoup(r.text,"lxml")
@@ -28,15 +33,20 @@ def login():
         assert errordiv is None
     except AssertionError as e:
         raise LoginException(errordiv.string.strip())
+    debug("Logged in successfully")
+    debug("Login PHPSESSID: "+r.cookies.get('PHPSESSID'), 3)
     return r.cookies
 
 def crawlfejezet(cookiejar, fcsop, fejezet=None):
     global jar
     kerdesgy=[]
+    debug('Entering category "'+fcsop['title']+(('", subcategory "'+fejezet['title']+'"') if fejezet else '"'), 1)
     params={"fcsop[fcsop]":fcsop['fcsop'], "fcsop[fejezet]":fejezet['fej'] if fejezet else None, "feladatcsoport_kerdes_lista[kerdes_pager][pg]": 1}
     gotqs=20
     while gotqs>0:
+        debug('Loading page '+str(params["feladatcsoport_kerdes_lista[kerdes_pager][pg]"]), 2)
         r=requests.get(baseurl, params=params, cookies=cookiejar)
+        debug("Retrieved "+r.url, 3)
         r.encoding="utf-8"
         soup=BeautifulSoup(r.text, "lxml")
         kerdess=soup.find_all('div', class_='kerdes_reszletes_belso')
@@ -46,18 +56,24 @@ def crawlfejezet(cookiejar, fcsop, fejezet=None):
             if(pvf):
                 ktype=pvf.table['title']
             else:
+                debug("\tFound pairing question", 4)
                 kerdesgy.append(pairing(kerdes))
                 continue
             if(ktype==u"I. Egyszerü feleletválasztás GY."):
+                debug("\tFound simple question", 4)
                 kerdesgy.append(simplechoice(kerdes))
             elif(ktype==u"II. Többszörös feleletválasztás GY"):
+                debug("\tFound multiple choice question", 4)
                 kerdesgy.append(multiplechoice(kerdes))
             elif(ktype==u"IV. Relációanalizis GY"):
+                debug("\tFound relanal question", 4)
                 kerdesgy.append(relanal(kerdes))
             else:
-                print("Ismeretlen feladattípus: "+ktype)
-                exit()
+                raise Exception("Ismeretlen kérdéstípus!")
+        debug(str(gotqs)+' questions gathered on this page.', 2)
         params["feladatcsoport_kerdes_lista[kerdes_pager][pg]"]+=1
+    debug(str(len(kerdesgy))+' questions gathered in this '+('sub' if fejezet else '')+'category.', 1)
+    debug('Exiting category '+fcsop['title']+((", subcategory "+fejezet['title']) if fejezet else ""), 2)
     r=requests.get(baseurl, params={"fcsop[vissza]":'true'}, cookies=cookiejar)
     return kerdesgy
 
@@ -65,25 +81,20 @@ def getfcsops(cookiejar):
     r=requests.get(baseurl, cookies=cookiejar)
     r.encoding="utf-8"
     soup=BeautifulSoup(r.text,"lxml")
-    return [{'title': div.a.string.strip(), 'fcsop':fcsop_re.search(div.a['href']).group(1)} for div in soup.find_all("div", class_="feladatcsoportok") if fcsop_re.search(div.a['href'])][0:2]
+    return [{'title': div.a.string.strip(), 'fcsop':fcsop_re.search(div.a['href']).group(1)} for div in soup.find_all("div", class_="feladatcsoportok") if fcsop_re.search(div.a['href'])][8:10]
 
-def getfejezets(fcsops, cookiejar):
-    tasks=[]
-    for fcsop in fcsops:
-        r=requests.get(baseurl, params={"fcsop[fcsop]":fcsop['fcsop']}, cookies=cookiejar)
-        r.encoding="utf-8"
-        soup=BeautifulSoup(r.text,"lxml")
-        fejezets=[{'title': div.a.string.strip(), 'fej':fejezetek_re.search(div.a['href']).group(1)} for div in soup.find_all("div", class_="feladatcsoportok") if fejezetek_re.search(div.a['href'])]
-        if(fejezets):
-            print(str(len(fejezets))+u' fejezetet találtam a '+fcsop['title']+u' kategóriában.')
-            for fejezet in fejezets:
-                print(fejezet['fej']+u': '+fejezet['title'])
-                tasks.append({'fcsop':fcsop, 'fejezet':fejezet})
-        else:
-            print(u'Nem találtam fejezeteket a '+fcsop['title']+u' kategóriában.')
-            tasks.append({'fcsop':fcsop, 'fejezet': None})
-        r=requests.get(baseurl, params={"fcsop[vissza]":'true'}, cookies=cookiejar)
-    return tasks
+def getfejezets(fcsop, cookiejar):
+    r=requests.get(baseurl, params={"fcsop[fcsop]":fcsop['fcsop']}, cookies=cookiejar)
+    r.encoding="utf-8"
+    soup=BeautifulSoup(r.text,"lxml")
+    fejezets=[{'title': div.a.string.strip(), 'fej':fejezetek_re.search(div.a['href']).group(1)} for div in soup.find_all("div", class_="feladatcsoportok") if fejezetek_re.search(div.a['href'])]
+    if(fejezets):
+        debug('Found '+str(len(fejezets))+' subcategories in category "'+fcsop['title']+u'"')
+        return fejezets
+    else:
+        print(u'No subcategories found in ctegory "'+fcsop['title']+u'"')
+        return [None]
+    r=requests.get(baseurl, params={"fcsop[vissza]":'true'}, cookies=cookiejar)
 
 
 def simplechoice(div):
@@ -99,6 +110,9 @@ def simplechoice(div):
     except Exception as e:
         print(div)
         raise e
+
+def simplechoicets(k):
+    return r'\multicolumn{3}{|p{\textwidth}|}{'+latex(k['leiras'])+"}\\\\\n"+'\\\\\n'.join([latex(v[0])+r"&\multicolumn{2}{p{\textwidth - .5cm - 6pt}|}{"+latex(v[1])+r'}' for v in k['valaszok']])+"\\\\\n\\hline\n"
 
 def multiplechoice(div):
     try:
@@ -140,20 +154,85 @@ def pairing(div):
                     } for tr in div.select('div.asszociacios_feladat table:nth-of-type(1) tr')],
                 'valaszok':[[td.get_text(strip=True).replace('\xa0',' ').strip() for td in [tr.contents[0],tr.contents[2]]] for tr in div.select('div.asszociacios_feladat table:nth-of-type(2) tr')]
                 }
+        kerdes['sorszam']=kerdes['kerdesek'][0]['sorszam']
         return kerdes
     except Exception as e:
         print(div)
         raise e
 
+typesetters=[lambda x:"", simplechoicets, lambda x:"", lambda x:"", lambda x:""]
+
+def latex(sz):
+    replacements = {r'#':r'\#',
+               r'$':r'\$',
+               r'%':r'\%',
+               r'^':r'\^{}',
+               r'&':r'\&',
+               r'_':r'\_',
+               r'{':'\{',
+               r'}':'\}',
+               r'~':r'$\sim$',
+               '\\':r'\textbackslash',
+               'μ':'u',
+               'α':'a'}
+    substrs = sorted(replacements.keys(), key=len, reverse=True)
+    regexp = re.compile('|'.join(map(re.escape, substrs)))
+    return regexp.sub(lambda match: replacements[match.group(0)], sz)
+
+def debug(info, level=1):
+    if(args.verbose>=level):
+        print((("debug "+str(level)+": ") if args.verbose>1 else "")+str(info), file=sys.stderr)
+
 def main():
-    try:
-        cookiejar=login()
-    except LoginException as e:
-        print("Nem sikerült bejelentkezni. A weboldal üzenete: "+str(e))
+    if(args.typeset_only is None):
+        try:
+            cookiejar=login()
+        except LoginException as e:
+            print("Nem sikerült bejelentkezni. A weboldal üzenete: "+str(e))
+            exit()
+        fcsops=getfcsops(cookiejar)
+        tasks=[{'fcsop':fcsop, 'fejezets':getfejezets(fcsop, cookiejar)} for fcsop in fcsops]
+        kerdesek=[{'fcsop':task['fcsop'], 'fejezets':[{'fejezet':fejezet, 'kerdesek':crawlfejezet(cookiejar, task['fcsop'], fejezet)} for fejezet in task['fejezets']]} for task in tasks]
+    else:
+        with open(args.typeset_only, 'r') as f:
+            debug('Opening file '+args.typeset_only)
+            kerdesek=json.load(f)
+    if(args.retrieve_only):
+        with open(args.retrieve_only, 'w') as f:
+            debug('Writing '+args.retrieve_only)
+            json.dump(kerdesek, f)
+            debug('Exiting without typesetting')
         exit()
-    tasks=getfejezets(getfcsops(cookiejar),cookiejar)
-    kerdesek=[{'focim':task['fcsop']['title'], 'alcim':task['fejezet']['title'] if task['fejezet'] else None, 'kerdesek':crawlfejezet(cookiejar, task['fcsop'], task['fejezet'])} for task in tasks]
-    print(kerdesek)
+    debug('Typesetting')
+    with open(args.output, 'w') as f:
+        f.write(r'''\documentclass{book}
+\usepackage[utf8]{inputenc}
+\usepackage[magyar]{babel}
+\usepackage{t1enc}
+\usepackage{tabularx}
+\usepackage{calc}
+\pagestyle{headings}
+\setlength{\parindent}{0pt}
+\begin{document}
+''')
+        for fcsop in kerdesek:
+            f.write(r'\chapter{'+fcsop['fcsop']['title']+'}\n')
+            for fejezet in fcsop['fejezets']:
+                if(fejezet['fejezet']):
+                    f.write(r'\section{'+fejezet['fejezet']['title']+'}\n')
+                    for kerdes in fejezet['kerdesek']:
+                        f.write('\\begin{tabular}{|p{.5cm}p{\\textwidth - 1.5cm - 24.8pt}p{1cm}|}\n\\hline\n\\multicolumn{3}{|p{\\textwidth}|}{'+kerdes['sorszam']+'}\\\\\n\\hline\n')
+                        f.write(typesetters[kerdes['type']](kerdes))
+                        f.write('\\end{tabular}\n\n')
+        f.write(r'\end{document}')
+    
 
 if __name__ == "__main__":
+    parser=argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', help='Bőbeszédű mód hibakereséshez', action="count", default=0)
+    group=parser.add_mutually_exclusive_group()
+    group.add_argument('-r', '--retrieve-only', help='Csak a kérdések letöltése és kiírása .json formátumba', metavar="FILENAME", nargs='?', const='zarovizsgakerdesek.json', default=None)
+    group.add_argument('-t', '--typeset-only', help='A letöltött .json formátumból .pdf készítése', metavar="FILENAME", nargs='?', const='zarovizsgakerdesek.json', default=None)
+    parser.add_argument('-o', '--output', help='Kimeneti fájl', default='zarovizsga.tex')
+    args=parser.parse_args()
     main()
